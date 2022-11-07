2bad170bed0c471e5200dc7d5b96211c68c8c921
Diego Hernandez <dnhernandez@my.waketech.edu>
2022-11-06 23:28:02 -0500

    Updates main.py file
    This commit updates the main.py file to query the duckduckgo API for a list of the Presidents of the United States.

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ verify the accuracy of the returned results.
 """
 import requests
 import json
-
+"""
 params = {
     "engine": "duckduckgo",
     "q": "Presidents of the united states",
     "output": "JSON",
     "api_key": "994b429f5f2d6895d2f69d0bfb16d7a5f9bb1f55c73da8ae8882fead2e3a33f8"
 }
-
+"""
 response = requests.get(
-    "https://serpapi.com/search.json?engine=duckduckgo&q=presidents&of&the&united&states",
-    params)
+    "https://api.duckduckgo.com/?q=presidents+of+the+united+states&format=json&pretty=1&no_html=1&skip_disambig=1")
 print(response.status_code)
 print(response.text)
 data = json.loads(response.text)
-for key in data.keys():
-    print(key)
